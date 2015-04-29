30d4421908a5628303f4f71935fe0e3e44d4aa96
App Academy Student <student@appacademy.io>
2015-04-29 12:02:48 -0700

    Slideable works, still -- refactoring

--- lib/game.rb
+++ lib/game.rb
@@ -16,12 +16,13 @@ end
 b = Board.new(true)
 
 
-bish = Bishop.new([7,7], b, :white)
+bish = Bishop.new([4,4], b, :white)
 k = King.new([0,0], b,  :black)
-
+puts "KINGS"
 p k.possible_moves
 #
-bish.possible_moves
+puts "BISHOPS"
+p bish.possible_moves.inspect
 #
 # p b.occupied?([0,0])
 # p b.piece_at([0,0])
