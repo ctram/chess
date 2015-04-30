de7d99c36aad3f6d5a6434f0f45ec83e056f13f7
App Academy Student <student@appacademy.io>
2015-04-29 17:22:59 -0700

    Finished chess logic.

--- lib/board.rb
+++ lib/board.rb
@@ -14,27 +14,47 @@ class Board
 
   def []=(pos, piece)
     x, y = pos
-    @grid[x][y]= piece
+    @grid[x][y] = piece
+  end
+
+  def checkmate?(color)
+    # if incheck, need to check whether the king's color's pieces include possible moves that will lead to a board state that is not in check.
+
+    friendly_pieces = find_pieces_of_color(color)
+    # friendly_possible_moves = []
+    friendly_pieces.any? do |piece|
+      piece.possible_moves.any? do |pos|
+        new_board = self.dup_board
+        x = piece.pos[0] # piece's current position
+        y = piece.pos[1]
+        new_board.grid[x][y].move(pos)
+        !new_board.in_check?(color)
+      end
+    end
   end
 
   def in_check?(color)
-    king_pos = find_king(color)
+    king_pos = find_king(color).pos
     color = (color == :black ? :white : :black)
     enemy_pieces = find_pieces_of_color(color)
-    byebug
     enemy_possible_moves = []
     enemy_pieces.each do |piece|
       enemy_possible_moves += piece.possible_moves
     end
-    byebug
+    # byebug
 
     enemy_possible_moves.include?(king_pos) ? true : false
   end
 
-  def checkmate?(color)
-    what color?
-    king.possible_moves
-  end
+  # def checkmate?(color)
+  #
+  #   king = find_king(color)
+  #   king.possible_moves.none? do |pos|
+  #     new_board = self.dup
+  #     new_board.find_king(color).move(pos)
+  #     new_board.in_check?(color)
+  #   end
+  # end
 
   def dup_board
     new_board = Board.new(false)
@@ -50,7 +70,7 @@ class Board
   end
 
   def fill_back_rows(color)
-    back_pieces = [Rook, Bishop, Knight, King, Queen, Knight, Bishop, Rook]
+    back_pieces = [Rook, Bishop, Knight, Queen, King, Knight, Bishop, Rook]
     back_pieces.each_with_index do |piece, col|
       row = color == :black ? 0 : 7
       @grid[row][col] = piece.new([row, col], self, color)
@@ -78,14 +98,20 @@ class Board
 
 
   def find_pieces_of_color(color)
-    enemy_pieces = @grid.flatten.compact.select{|piece| piece.color == color}
+    pieces = @grid.flatten.compact.select{|piece| piece.color == color}
   end
 
   def find_king(color)
+    # begin
     king = @grid.flatten.compact.find do |piece|
       piece.is_a?(King) && piece.color == color
     end
-    king.pos
+    if king == nil
+
+      raise "No King found"
+    else
+      king
+    end
   end
 
 
--- lib/pieces/stepable.rb
+++ lib/pieces/stepable.rb
@@ -5,8 +5,11 @@ module Stepable
     directions.each do |dir|
       del_x, del_y = dir
       cur_x, cur_y = pos
-
-      positions << [del_x + cur_x, del_y + cur_y]
+      new_x, new_y = del_x + cur_x, del_y + cur_y
+      new_pos = [new_x, new_y]
+      if board.on_board?(new_pos)
+        positions << [new_x, new_y]
+      end
     end
 
     positions
